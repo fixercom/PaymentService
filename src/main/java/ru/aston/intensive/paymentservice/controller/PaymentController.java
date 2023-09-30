a885809b18a3bdf054897b454159f000ff5873ca
Alexander Matushin <fixercom@yandex.ru>
2023-09-30 15:27:17 +0300

    feat: create migration script for payments table, add base structure

---
+++ src/main/java/ru/aston/intensive/paymentservice/controller/PaymentController.java
@@ -0,0 +1,9 @@
+package ru.aston.intensive.paymentservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PaymentController {
+}
